f19fc8854e8e96a8c507a32106018b9a555a3727
oliviakris24 <okfishy@gmail.com>
2018-07-12 22:47:37 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,16 +1,8 @@
 # person.rb
-class Person 
 
-def initialize(name)
-  @name = name 
-end 
-
-def name
-  @name = "Beyonce" 
-end 
-
-def name=(new_name)
-  @name = new_name
-end 
-
-end 
+class Person
+ def initialize(name)
+   @name = name
+   
+ end
+end
